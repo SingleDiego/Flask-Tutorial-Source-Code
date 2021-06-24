94aa2b8cb5e8d8a465837fab77666b3af4f4b96c
SingleDiego <768688350@qq.com>
2021-06-24 16:18:49 +0800

    Add files via upload

--- app/routes.py
+++ app/routes.py
@@ -1,8 +1,26 @@
-from flask import Blueprint
+from flask import render_template, Blueprint
 
 main_routes = Blueprint('main', __name__)
 
-
 @main_routes.route('/')
+@main_routes.route('/index')
 def index():
-    return 'hello world!'
+    user = {'username': 'Diego'}
+
+    posts = [
+        {
+            'author': {'username': 'John'},
+            'body': 'Hello World!'
+        },
+        {
+            'author': {'username': 'Diego'},
+            'body': 'Hola Wolrd!'
+        }
+    ]
+
+    return render_template(
+        'index.html', 
+        title='Home', 
+        user=user, 
+        posts=posts
+    )
