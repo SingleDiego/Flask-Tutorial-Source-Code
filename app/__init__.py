55804a9357410269a67d7b8377d4198ee2cc2a8b
SingleDiego <768688350@qq.com>
2021-06-28 10:38:47 +0800

    Add files via upload

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,11 @@
 from flask import Flask
+from config import Config
 
 def create_app():
     app = Flask(__name__)
+
+    # 加载配置
+    app.config.from_object(Config)
     
     # 引入蓝图并注册
     from app.routes import main_routes
