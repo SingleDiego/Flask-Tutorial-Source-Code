4c066c84c2bac8541858389cb4148bae2dbe12c5
SingleDiego <768688350@qq.com>
2021-07-01 08:22:48 +0800

    Add files via upload

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,28 @@
 from flask import Flask
+from config import Config
+
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+
+
+db = SQLAlchemy()
+migrate = Migrate()
+
 
 def create_app():
     app = Flask(__name__)
+
+    # 加载配置
+    app.config.from_object(Config)
+    
+    # 初始化各种扩展库
+    db.init_app(app)
+    migrate.init_app(app, db)
     
     # 引入蓝图并注册
     from app.routes import main_routes
     app.register_blueprint(main_routes)
 
     return app
+
+from app import models
--- app/routes.py
+++ app/routes.py
@@ -1,8 +1,44 @@
-from flask import Blueprint
+from flask import (
+    render_template, 
+    Blueprint, 
+    flash, 
+    redirect,
+    url_for
+)
+from app.forms import LoginForm
+
 
 main_routes = Blueprint('main', __name__)
 
-
 @main_routes.route('/')
+@main_routes.route('/index')
 def index():
-    return 'hello world!'
+    user = {'username': 'Diego'}
+
+    posts = [
+        {
+            'author': {'username': 'John'},
+            'body': 'Hello World!'
+        },
+        {
+            'author': {'username': 'Diego'},
+            'body': 'Hola Wolrd!'
+        }
+    ]
+
+    return render_template(
+        'index.html', 
+        title='Home', 
+        user=user, 
+        posts=posts
+    )
+
+
+@main_routes.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash('Login requested for user {}, remember_me={}'.format(
+            form.username.data, form.remember_me.data))
+        return redirect(url_for('main.index'))
+    return render_template('login.html', title='Sign In', form=form)
