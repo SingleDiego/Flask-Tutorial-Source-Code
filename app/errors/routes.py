57afcc54c793c1e56326f4730a3ed530b15b5a4a
SingleDiego <768688350@qq.com>
2021-07-09 23:35:27 +0800

    Update routes.py

--- app/errors/routes.py
+++ app/errors/routes.py
@@ -3,11 +3,11 @@ from app import db
 from app.errors import errors_routes
 
 
-@errors_routes.errorhandler(404)
+@errors_routes.app_errorhandler(404)
 def not_found_error(error):
     return render_template('errors/404.html'), 404
 
-@errors_routes.errorhandler(500)
+@errors_routes.app_errorhandler(500)
 def internal_error(error):
     db.session.rollback()
     return render_template('errors/500.html'), 500
