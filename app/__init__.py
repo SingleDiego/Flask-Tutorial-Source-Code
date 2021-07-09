2aee1dbcdf7f27c3ab1ac7e0ff8221d46b39431f
SingleDiego <768688350@qq.com>
2021-07-09 17:28:08 +0800

    Add files via upload

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,35 @@
 from flask import Flask
+from config import Config
+
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from flask_login import LoginManager
+
+db = SQLAlchemy()
+migrate = Migrate()
+login = LoginManager()
 
 def create_app():
     app = Flask(__name__)
+
+    # 加载配置
+    app.config.from_object(Config)
     
+    # 初始化各种扩展库
+    db.init_app(app)
+    migrate.init_app(app, db)
+    login.init_app(app)
+
     # 引入蓝图并注册
-    from app.routes import main_routes
+    from app.main.routes import main_routes
     app.register_blueprint(main_routes)
 
+    from app.auth.routes import auth_routes
+    app.register_blueprint(auth_routes)
+
+    from app.errors.routes import errors_routes
+    app.register_blueprint(errors_routes)
+
     return app
+
+from app import models
---
+++ app/auth/routes.py
@@ -0,0 +1,89 @@
+from flask import (
+    render_template, 
+    flash, 
+    redirect,
+    url_for,
+    request
+)
+from flask_login import (
+    login_user, 
+    logout_user, 
+    current_user,
+    login_required
+)
+from app.auth.forms import (
+    LoginForm, 
+    RegistrationForm, 
+    EditProfileForm
+)
+from app import db
+from app.models import User
+from app.auth import auth_routes
+
+
+@auth_routes.route('/login', methods=['GET', 'POST'])
+def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('main.index'))
+
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(username=form.username.data).first()
+        if user is None or not user.check_password(form.password.data):
+            flash('Invalid username or password')
+            return redirect(url_for('auth.login'))
+        login_user(user, remember=form.remember_me.data)
+        return redirect(url_for('main.index'))
+    return render_template('auth/login.html', title='Sign In', form=form)
+
+@auth_routes.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('main.index'))
+
+@auth_routes.route('/register', methods=['GET', 'POST'])
+def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('auth.index'))
+
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(username=form.username.data, email=form.email.data)
+        user.set_password(form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        flash('Congratulations, you are now a registered user!')
+        return redirect(url_for('auth.login'))
+    return render_template('auth/register.html', title='Register', form=form)
+
+@auth_routes.route('/user/<username>')
+@login_required
+def user(username):
+    user = User.query.filter_by(username=username).first_or_404()
+    posts = [
+        {'author': user, 'body': 'Test post #1'},
+        {'author': user, 'body': 'Test post #2'}
+    ]
+    return render_template('auth/user.html', user=user, posts=posts)
+
+@auth_routes.route('/edit_profile', methods=['GET', 'POST'])
+@login_required
+def edit_profile():
+    form = EditProfileForm()
+
+    if form.validate_on_submit():
+        current_user.username = form.username.data
+        current_user.about_me = form.about_me.data
+        db.session.commit()
+        flash('Your changes have been saved.')
+        return redirect(url_for('auth.edit_profile'))
+
+    elif request.method == 'GET':
+        form.username.data = current_user.username
+        form.about_me.data = current_user.about_me
+
+    return render_template(
+        'auth/edit_profile.html', 
+        title='Edit Profile',
+        form=form
+    )
