c7e17e02528522e051f98dda2cd40c76f3940223
SingleDiego <768688350@qq.com>
2021-08-02 16:38:05 +0800

    Add files via upload

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,66 @@
 from flask import Flask
+from config import Config
 
-def create_app():
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from flask_login import LoginManager
+from flask_mail import Mail
+from flask_bootstrap import Bootstrap
+
+import os
+import logging
+from logging.handlers import RotatingFileHandler
+
+
+db = SQLAlchemy()
+migrate = Migrate()
+mail = Mail()
+bootstrap = Bootstrap()
+
+login = LoginManager()
+login.login_view = "auth.login"
+login.login_message = "Please log in to access this page."
+
+def create_app(config):
     app = Flask(__name__)
+
+    # 加载配置
+    app.config.from_object(config)
     
+    # 初始化各种扩展库
+    db.init_app(app)
+    migrate.init_app(app, db)
+    login.init_app(app)
+    mail.init_app(app)
+    bootstrap.init_app(app)
+
+    # 日志设定
+    if not app.debug:
+        if not os.path.exists('logs'):
+            os.mkdir('logs')
+
+        app.logger.setLevel(logging.INFO)
+        handler = RotatingFileHandler(
+            'logs/microblog.log',
+            maxBytes=10240, 
+            backupCount=10
+        )
+        handler.setLevel(logging.INFO)
+        formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+        handler.setFormatter(formatter)
+        app.logger.addHandler(handler)
+        app.logger.info("Microblog startup!")
+
     # 引入蓝图并注册
-    from app.routes import main_routes
+    from app.main.routes import main_routes
     app.register_blueprint(main_routes)
 
+    from app.auth.routes import auth_routes
+    app.register_blueprint(auth_routes)
+
+    from app.errors.routes import errors_routes
+    app.register_blueprint(errors_routes)
+
     return app
+
+from app import models
---
+++ app/main/routes.py
@@ -0,0 +1,67 @@
+from flask import (
+    render_template, 
+    flash,
+    redirect,
+    url_for,
+    request,
+    current_app
+)
+from flask_login import current_user, login_required
+from app import db
+from app.models import Post
+from app.main import main_routes
+from app.main.forms import PostForm
+# from app.email import send_email
+
+
+@main_routes.route('/', methods=['GET', 'POST'])
+@main_routes.route('/index', methods=['GET', 'POST'])
+@login_required
+def index():
+    form = PostForm()
+    if form.validate_on_submit():
+        post = Post(body=form.post.data, author=current_user)
+        db.session.add(post)
+        db.session.commit()
+        flash('Your post is now live!')
+        return redirect(url_for('main.index'))
+
+    page = request.args.get('page', 1, type=int)
+    posts_pre_page = current_app.config['POSTS_PER_PAGE']
+    posts = current_user.followed_posts() \
+        .paginate(page, posts_pre_page, False)
+    next_url = url_for('main.index', page=posts.next_num) \
+        if posts.has_next else None
+    prev_url = url_for('main.index', page=posts.prev_num) \
+        if posts.has_prev else None
+
+    return render_template(
+        'index.html', 
+        title='Home Page', 
+        form=form,
+        posts=posts.items,
+        next_url=next_url,
+        prev_url=prev_url
+    )
+
+@main_routes.route('/explore')
+@login_required
+def explore():
+    page = request.args.get('page', 1, type=int)
+    posts_pre_page = current_app.config['POSTS_PER_PAGE']
+    posts = Post.query \
+        .order_by(Post.timestamp.desc()) \
+        .paginate(page, posts_pre_page, False)
+    next_url = url_for('main.explore', page=posts.next_num) \
+        if posts.has_next else None
+    prev_url = url_for('main.explore', page=posts.prev_num) \
+        if posts.has_prev else None
+
+    return render_template(
+        'index.html', 
+        title='Explore', 
+        posts=posts.items,
+        next_url=next_url,
+        prev_url=prev_url
+    )
+
